3c6df0f70686b434982c991d5ad1197759ea5431
damanM <daman.m@outlook.com>
2023-03-30 00:57:18 -0400

    changed heading font and background to a gradiant + shorter how to play instructions

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -8,11 +8,10 @@ const Home = () => {
                 <section className='howToPlay'>
                     <div className='howToFlex'>
                         <h2>How To Play</h2>
-                        <p>
-                        This is a learning game. Players will be presented with two randomly generated homophonous words - like piece and peace - and a single definition. Players must match the correct spelling to the definition. 
-                        There are up to 10 questions. Each correct answer is worth 1 point. However, if a player chooses the wrong homophonous, they loose -0.5 points. 
-                        At the end, players have the option to save their score to the leaderboard! 
-                        </p>
+
+                        <p><span>1. </span>Players are presented with two random homophonous words & one definition</p>
+                        <p><span>2. </span>Match the correct homophone to the definition </p>
+                        <p><span>3. </span>Get smarter</p>
                         <div className='btnDiv'>
                                 <Link  className='play 'to="/GamePage">Play Now!</Link>
                         </div>
